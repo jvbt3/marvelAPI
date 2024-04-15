221e2366a26ea896a7cb26068434438331dac823
jvbt3 <joaovitormartins077@gmail.com>
2024-04-15 14:36:30 -0300

    Get all API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreatorsModule } from './creators/creators.module';
 import { ComicsModule } from './comics/comics.module';
 import { CharacterModule } from './character/character.module';
 import { HttpModule } from '@nestjs/axios';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [CreatorsModule, ComicsModule, CharacterModule, HttpModule],
